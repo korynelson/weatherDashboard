bf516fcb6ec9a258f8f2bf10d0139c75c2eda070
Kory Nelson <kory.nelson99@gmail.com>
2020-09-14 01:15:45 -0700

    added preventDefault for button click search.  linked the 2 ajax functions so the lat'lng feeds into weather app

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,64 @@
-$("searchBtn").on("click",function(){
+$(document).ready(function(){
 
-    $.ajax({
-        url: "https://api.opencagedata.com/geocode/v1/json?q=seattle,wa&key=29af6675a09045828a1222f7eabddf65",
-        method:"GET"
-    })
-    .then(function(response) {
-        console.log(response.results[0].geometry);
-        ajaxCall2(response.results[0].geometry.lat,response.results[0].geometry.lng)
+    $("#searchBtn").on("click",function(event){
+        //this stips the button from submitting the form
+        event.preventDefault();
+        var location = ($(this)[0].previousSibling.previousSibling.value);
+        //run the ajax to get city lat/lng
+        ajaxCall1(location);
     });
 
-})
+    function ajaxCall1(loc){
+        console.log(loc)
+        var APIKey = "29af6675a09045828a1222f7eabddf65";
+        var queryURL = "https://api.opencagedata.com/geocode/v1/json?q="+loc+"&key="+ 
+        APIKey 
+        + "&language=en&pretty=1";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+              
+        // Log the resulting object
+        console.log(response.results[0].geometry.lat,response.results[0].geometry.lng);
+        ajaxCall2(response.results[0].geometry.lat,response.results[0].geometry.lng)
+      });
 
-
-//Query Weather Data From OpenWeather
-function ajaxCall2(lat,lng){
-    console.log(lat)
-    console.log(lng)
-
-    var APIKey = "d76a54ec670a9ff8425f5e3466754232";
-    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?"+"lat="+ lat + "&lon="+ lng +
-    "&exclude={minute,hourly}&appid="+APIKey;
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-  .then(function(response) {
-
-    // Log the resulting object
-    console.log(response);
-
-    //Put new object in local storage
-    window.localStorage.setItem("weatherArray",JSON.stringify(response))
-
-    // Transfer content to HTML
-    $("#city").html("<h1>Current Weather</h1>");
-    $("#wind").text("Wind Speed: " + response.current.wind_speed);
-    $("#humidity").text("Humidity: " + response.current.humidity);
+    }
+        // ajaxCall2(response.results[0].geometry.lat,response.results[0].geometry.lng)
     
-    // Convert the temp to fahrenheit
-    var tempF = (response.current.temp - 273.15) * 1.80 + 32;
+    //Query Weather Data From OpenWeather
+    function ajaxCall2(lat,lng){
+        console.log(lat)
+        console.log(lng)
 
-    // add temp content to html
-    $("#temp").text("Temperature (K) " + response.current.temp);
-    $("#tempF").text("Temperature (F) " + tempF.toFixed(2));
-  });
-}
+        var APIKey = "d76a54ec670a9ff8425f5e3466754232";
+        var queryURL = "https://api.openweathermap.org/data/2.5/onecall?"+"lat="+ lat + "&lon="+ lng +
+        "&exclude={minute,hourly}&appid="+APIKey;
 
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+
+        // Log the resulting object
+        console.log(response);
+
+        //Put new object in local storage
+        window.localStorage.setItem("weatherArray",JSON.stringify(response))
+
+        // Transfer content to HTML
+        $("#city").html("<h1>Current Weather</h1>");
+        $("#wind").text(`Wind Speed: ${response.current.wind_speed}`);
+        $("#humidity").text("Humidity: " + response.current.humidity);
+        
+        // Convert the temp to fahrenheit
+        var tempF = (response.current.temp - 273.15) * 1.80 + 32;
+
+        // add temp content to html
+        $("#temp").text("Temperature (K) " + response.current.temp);
+        $("#tempF").text("Temperature (F) " + tempF.toFixed(2));
+      });
+    }
+
+});
